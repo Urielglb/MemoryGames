e34074a33020e5f251ea26f991dff4449f50b71b
Uriel García Luna Bobadilla <uriel_06@ciencias.unam.mx>
2020-12-18 23:36:56 -0600

    Juego citibanamex

--- src/js/imgs.js
+++ src/js/imgs.js
@@ -1,11 +1,18 @@
-import first from '../assets/imgs/1.jpeg';
-import second from '../assets/imgs/2.jpeg';
-import third from '../assets/imgs/3.jpeg';
-import fourth from '../assets/imgs/4.jpeg';
-import fifth from '../assets/imgs/5.jpeg';
-import sixth from '../assets/imgs/6.jpeg';
-import seventh from '../assets/imgs/7.jpeg';
-import eighth from '../assets/imgs/8.jpeg';
+import first from '../assets/imgs/1.jpg';
+import second from '../assets/imgs/2.jpg';
+import third from '../assets/imgs/3.jpg';
+import fourth from '../assets/imgs/4.jpg';
+import fifth from '../assets/imgs/5.jpg';
+import sixth from '../assets/imgs/6.jpg';
+import seventh from '../assets/imgs/7.jpg';
+import eighth from '../assets/imgs/8.jpg';
+import nineth from '../assets/imgs/9.jpg';
+import tenth from '../assets/imgs/10.jpg';
+import eleventh from '../assets/imgs/11.jpg';
+import twelfth from '../assets/imgs/12.jpg';
+import thirteenth from '../assets/imgs/13.jpg';
+import  fourteenth from '../assets/imgs/14.jpg';
+import fifteenth from '../assets/imgs/15.jpg';
 const imgs =[
     {
         "src" : first,
@@ -38,6 +45,34 @@ const imgs =[
     {
         "src" : eighth,
         "id": "eighth"
+    },
+    {
+        "src" : nineth,
+        "id": "nineth"
+    },
+    {
+        "src" : tenth,
+        "id": "tenth"
+    },
+    {
+        "src" : eleventh,
+        "id": "eleventh"
+    },
+    {
+        "src" : twelfth,
+        "id": "twelfth"
+    },
+    {
+        "src" : thirteenth,
+        "id": "thirteenth"
+    },
+    {
+        "src" : fourteenth,
+        "id": "fourteenth"
+    },
+    {
+        "src" : fifteenth,
+        "id": "fifteenth"
     }
 ]
 
